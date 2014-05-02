e210b737b15f772992198cb1406a4df7566eda6f
Seb Bacon <seb.bacon@gmail.com>
2014-05-02 11:15:26 +0100

    Use the Opencorporates Docker container for executing ruby scripts.
    Allows us to wrap scrapers in custom database-saving code

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -147,7 +147,7 @@ class Scraper < ActiveRecord::Base
 
   def self.update_docker_image!
     docker_command = "docker #{ENV['DOCKER_TCP'] ? "-H #{ENV['DOCKER_TCP']}" : ""}"
-    system("#{docker_command} pull openaustralia/morph-ruby")
+    system("#{docker_command} pull opencorporates/morph-ruby")
     system("#{docker_command} pull openaustralia/morph-php")
     system("#{docker_command} pull openaustralia/morph-python")
     system("#{docker_command} pull openaustralia/morph-perl")
