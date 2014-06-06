e37805d5b1f2cdaa8bc365350d23e6b263da285c
Peter Inglesby <peter.inglesby@gmail.com>
2014-06-06 15:07:51 +0100

    Log when DelayedRunWorker performs

--- app/workers/delayed_run_worker.rb
+++ app/workers/delayed_run_worker.rb
@@ -4,6 +4,7 @@ class DelayedRunWorker
 
   def perform(scraper_id)
     scraper = Scraper.find(scraper_id)
+    Rails.logger.info("DelayedRunWorker performing with scraper #{scraper.name}")
     # Guard against more than one of a particular scraper running at the same time
     if scraper.runnable?
       run = scraper.runs.create(queued_at: Time.now, auto: true, owner_id: scraper.owner_id)
