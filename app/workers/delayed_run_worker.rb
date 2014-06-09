6bbcb89d2fcd25b83cc34280a7e35ae4756fbef3
Peter Inglesby <peter.inglesby@gmail.com>
2014-06-09 14:49:23 +0100

    Fix DelayedRunWorker#perform

--- app/workers/delayed_run_worker.rb
+++ app/workers/delayed_run_worker.rb
@@ -2,14 +2,12 @@ class DelayedRunWorker
   include Sidekiq::Worker
   sidekiq_options queue: :low
 
-  def perform(scraper_id)
-    scraper = Scraper.find(scraper_id)
+  def perform(run_id)
+    Rails.logger.info("DelayedRunWorker performing with run #{run_id}")
+    run = Run.find(run_id)
+    scraper = run.scraper
     Rails.logger.info("DelayedRunWorker performing with scraper #{scraper.name}")
-    # Guard against more than one of a particular scraper running at the same time
-    if scraper.runnable?
-      run = scraper.runs.create(queued_at: Time.now, auto: true, owner_id: scraper.owner_id)
-      run.synch_and_go!
-    end
+    run.synch_and_go!
   end
 end
 
