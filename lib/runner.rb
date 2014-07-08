94ed968c585c0d5794b7166f3b541e60e0a833dc
Peter Inglesby <peter.inglesby@gmail.com>
2014-07-08 15:26:09 +0100

    Set LC_ALL in docker environment

--- lib/runner.rb
+++ lib/runner.rb
@@ -152,6 +152,7 @@ class Runner
       # On a 1G machine we're allowing a max of 10 containers to run at a time. So, 100M
       # TODO check this is right for openc use case
       'Memory' => 100.megabytes,
+      'Env' => ['LC_ALL=en_GB.UTF-8'],
     }
     Rails.logger.info("Creating container with params #{container_params}")
     Docker::Container.create(container_params, conn)
