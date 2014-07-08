9a2a04cb3651690942eaf846075bd7eb6012ccd4
Peter Inglesby <peter.inglesby@gmail.com>
2014-07-08 01:49:45 +0100

    Determine whether run succeeded by examining output

--- utils/wrapper.rb
+++ utils/wrapper.rb
@@ -22,6 +22,10 @@ class Runner < TurbotRunner::BaseRunner
   end
 
   def handle_failed_run
+    # This string is important.  We check for its presence in runner.rb to
+    # determine whether the run was successful.  It would be much better if we
+    # could check the return code of this script, but docker does not reliably
+    # pick up the return code, so we've got to improvise.
     STDERR.puts('Bot did not run to completion')
   end
 end
@@ -29,8 +33,9 @@ end
 runner = Runner.new('/repo')
 runner.run
 
-if runner.successful?
-  exit(0)
-else
-  exit(1)
-end
+# It'd be nice if this worked, but it doesn't.  See note above.
+#if runner.successful?
+#  exit(0)
+#else
+#  exit(1)
+#end
