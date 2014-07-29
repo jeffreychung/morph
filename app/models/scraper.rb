f22a5b0f806ef8e5b5350f38915e23cb03e65be6
Seb Bacon <seb.bacon@gmail.com>
2014-07-29 08:57:28 +0100

    Use correct (opencorporates) docker image when updating

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -149,7 +149,7 @@ class Scraper < ActiveRecord::Base
     docker_command = "docker #{ENV['DOCKER_TCP'] ? "-H #{ENV['DOCKER_TCP']}" : ""}"
     system("#{docker_command} pull opencorporates/morph-ruby")
     system("#{docker_command} pull openaustralia/morph-php")
-    system("#{docker_command} pull openaustralia/morph-python")
+    system("#{docker_command} pull opencorporates/morph-python")
     system("#{docker_command} pull openaustralia/morph-perl")
   end
 
