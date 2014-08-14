611a82bfa0338c62397006b6eed648bc8bcfa89c
Peter Inglesby <peter.inglesby@gmail.com>
2014-08-14 17:27:58 +0100

    Increase timeout to 24 hours

--- lib/turbot_docker_runner.rb
+++ lib/turbot_docker_runner.rb
@@ -148,7 +148,7 @@ class TurbotDockerRunner
 
   def create_container
     Rails.logger.info('Creating container')
-    conn = Docker::Connection.new(docker_url, read_timeout: 4.hours)
+    conn = Docker::Connection.new(docker_url, read_timeout: 24.hours)
     container_params = {
       'name' => "#{@bot_name}_#{@run_uid}",
       'Cmd' => ['/bin/bash', '-l', '-c', command],
