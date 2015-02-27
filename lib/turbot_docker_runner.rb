4f1d4d0faf528176e6d6e12b425332e7797fdef8
seb <seb.bacon@gmail.com>
2015-02-27 13:59:36 +0000

    Support a new run_type of "prescraped".
    This allows us to upload *.out files to a known location, and then
    trigger ingestion of those files.

--- lib/turbot_docker_runner.rb
+++ lib/turbot_docker_runner.rb
@@ -31,7 +31,11 @@ class TurbotDockerRunner
 
   def run
     set_up
-    status_code = run_in_container
+    if @run_type == 'prescraped'
+      status_code = 0
+    else
+      status_code = run_in_container
+    end
 
     process_output
 
@@ -59,6 +63,7 @@ class TurbotDockerRunner
     set_up_directory(output_path)
     set_up_directory(downloads_path)
     synchronise_repo
+    copy_prescraped_data_to_run if @run_type == "prescrape"
 
     clear_saved_vars if @run_type == 'first_of_scrape'
 
@@ -83,6 +88,12 @@ class TurbotDockerRunner
     Rails.logger.info("Setting up #{path}")
     FileUtils.mkdir_p(path)
     FileUtils.chmod(0777, path)
+  end
+
+  def copy_prescraped_data_to_run
+    Dir.glob(File.join(prescraped_path, 'output', 'data', '*.out')).each do |file|
+      FileUtils.cp(file, output_path)
+    end
   end
 
   def synchronise_repo
@@ -348,6 +359,13 @@ class TurbotDockerRunner
       base_path,
       'repo',
       @bot_name[0],
+      @bot_name
+    )
+  end
+
+  def prescraped_path
+    File.join(
+      PRESCRAPED_BOT_DIR,
       @bot_name
     )
   end
